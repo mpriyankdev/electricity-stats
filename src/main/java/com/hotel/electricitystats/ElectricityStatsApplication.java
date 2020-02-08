d9e04eb709dfb2f740e4f0979e6293e94ce698d6
mpriyankdev <mpriyank.dev@gmail.com>
2020-02-08 22:21:08 +0530

    Initial Commit

---
+++ src/main/java/com/hotel/electricitystats/ElectricityStatsApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.electricitystats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityStatsApplication.class, args);
+	}
+
+}
